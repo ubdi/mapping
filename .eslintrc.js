697082b822f94c17a065a88d685fd57643bb43bf
Harun Smrkovic <hakazvaka@gmail.com>
2020-03-04 12:03:27 +0100

    Move converter fn from insights

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-console': 'off'
+  }
+}
